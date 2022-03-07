07bc37d7f74e386c29a99da1b793854d1902ba29
Manuel <69088344+manueter@users.noreply.github.com>
2022-03-07 15:22:25 -0300

    Update tree.js
    Adding more trees and mouseY variable on l (largo).

--- tree.js
+++ tree.js
@@ -6,13 +6,24 @@ function draw()
 {
   background(77,107,83);
 
-  new Branch(width/2,height,map(mouseX,0,width,-PI/20,PI/2),80,0,5);
-  new Branch(width*3/10,height,map(mouseX,0,width,-PI/20,PI/2),100,0,5);
-  new Branch(width/10,height,map(mouseX,0,width,-PI/20,PI/2),70,0,5);
+  strokeWeight(2.5);
+  stroke(50,62,50);
+  new Branch(width/4,height,map(mouseX,0,width,-PI/20,PI/2),15,0,3);
+  new Branch(width*3/8,height,map(mouseX,0,width,-PI/20,PI/2),10,0,3);
+  new Branch(width*3/7,height,map(mouseX,0,width,-PI/20,PI/2),40,0,4);
   
-  new Branch(width*7/10,height,map(mouseX,0,width,-PI/20,PI/2),40,0,5);
+  new Branch(width*5/8,height,map(mouseX,0,width,-PI/20,PI/2),120,0,5);
+  new Branch(width*6/8,height,map(mouseX,0,width,-PI/20,PI/2),30,0,4);
+  new Branch(width*2/7,height,map(mouseX,0,width,-PI/20,PI/2),50,0,3);
+  
+  new Branch(width/2,height,map(mouseX,0,width,-PI/20,PI/2),map(mouseY,0,height,80,120),0,5);
+  
+  new Branch(width/10,height,map(mouseX,0,width,-PI/20,PI/2),70,0,5);
+  new Branch(width*2/10,height,map(mouseX,0,width,-PI/20,PI/2),map(mouseY*2,0,height,50,100),0,5);
+  new Branch(width*3/10,height,map(mouseX,0,width,-PI/20,PI/2),100,0,6);
+  new Branch(width*4/5,height,map(mouseX,0,width,-PI/20,PI/2),map(mouseY/2,0,height,50,100),0,6);
+  new Branch(width*7/10,height,map(mouseX,0,width,-PI/20,PI/2),40,0,4);
   new Branch(width*9/10,height,map(mouseX,0,width,-PI/20,PI/2),90,0,5);
-  new Branch(width*4/5,height,map(mouseX,0,width,-PI/20,PI/2),50,0,5);
 }
 
 class Branch
